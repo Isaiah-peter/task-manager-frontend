5bf00f342d8b6acdcaab54490b82f26e541eba84
Isaiah-peter <peterisaiah4fun@gmail.com>
2025-06-02 22:16:45 +0100

    side bar setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Register from './component/auth/Register'
 import Login from './component/auth/Login'
 import Navbar from './component/dashboard/navbar'
+import SideBar from './component/dashboard/sidebar'
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <SideBar />
     </>
   )
 }
---
+++ src/component/dashboard/sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "../../style/sidebar.css"
+
+const SideBar = () => {
+    return (
+        <div class="sidebar">
+
+        </div>
+    )
+}
+
+export default SideBar;
