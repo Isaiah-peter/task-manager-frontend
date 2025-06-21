f6b37af929ec3ed88e0679ea70e0a5b871b96b0b
Isaiah-peter <peterisaiah4fun@gmail.com>
2025-06-21 02:46:28 +0100

    addTask

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "../style/modal.css";
 
-const Modal = ({ show, onClose, children }) => {
+const Modal = ({ show, onClose, children, header }) => {
   if (!show) return null;
 
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-box" onClick={(e) => e.stopPropagation()}>
-        <span className="modal-close" onClick={onClose}>&times;</span>
+        <div className="header">
+          <h3>{header}</h3>
+          <span className="modal-close" onClick={onClose}>&times;</span>
+        </div>
         <div className="modal-content">{children}</div>
       </div>
     </div>
--- src/component/dashboard/main.jsx
+++ src/component/dashboard/main.jsx
@@ -1,11 +1,14 @@
 import React, {useState} from 'react'
-import "../../style/main.css"
 import handwave from "../../assets/handwave.svg"
 import friend from "../../assets/friend.svg"
 import pending from "../../assets/pending.svg"
 import CircularProgressBar from './CircularProgressBar';
 import taskIcon from "../../assets/task-icon.svg";
-import Modal from "../Modal"
+import Modal from "../Modal";
+import ImageUploader from './imageUploader';
+
+import "../../style/main.css"
+import "../../style/addtask.css"
 
 const Home = () => {
     const [showModal, setShowModal] = useState(false);
@@ -230,9 +233,57 @@ const Home = () => {
                 </div>
             </div>
 
-            <Modal show={showModal} onClose={() => setShowModal(false)}>
-                <h2>Hello!</h2>
-                <p>This is a simple modal built with React and CSS.</p>
+            <Modal show={showModal} header="Add New Task" onClose={() => setShowModal(false)}>
+                <div className="addTask-container">
+                    <form action="" className="addtask-wrapper">
+                        <div className="addtask-input-wrapper">
+                            <label htmlFor="" className="addtask-label">Title</label>
+                            <input type="text" className="addtask-input" />
+                        </div>
+                        <div className="addtask-input-wrapper">
+                            <label htmlFor="" className="addtask-label">Date</label>
+                            <input type="date" className="addtask-input" />
+                        </div>
+                        <div className="priority-container">
+                            <div className="priority-input">
+                                <label className="task-status-text">
+                                    <svg width="7" height="7" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="3.5" cy="3.5" r="3.5" fill="#05A301"/>
+                                    </svg>
+                                    <p>High</p>
+                                </label>
+                                <input type="checkbox" name="" id="" /> 
+                            </div>
+                            <div className="priority-input">
+                                <label className="task-status-text">
+                                    <svg width="7" height="7" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="3.5" cy="3.5" r="3.5" fill="#05A301"/>
+                                    </svg>
+                                    <p>High</p>
+                                </label>
+                                <input type="checkbox" name="" id="" /> 
+                            </div>
+                            <div className="priority-input">
+                                <label className="task-status-text">
+                                    <svg width="7" height="7" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="3.5" cy="3.5" r="3.5" fill="#05A301"/>
+                                    </svg>
+                                    <p>High</p>
+                                </label>
+                                <input type="checkbox" name="" id="" /> 
+                            </div>
+                        </div>
+                        <div className="task-discription">
+                            <div className="addtask-input-wrapper">
+                                <label htmlFor="" className="addtask-label"></label>
+                                <textarea type="text" className="addtask-input" />
+                            </div>
+                            <div className="upload-image">
+                                <ImageUploader />
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </Modal>
         </div>
     )
